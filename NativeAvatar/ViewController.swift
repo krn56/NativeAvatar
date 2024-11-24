5c0f0502ae21f8ed06cad75882fa20ab1770c921
Roman Krendyasov <krendyasov_rn@mail.ru>
2024-11-24 20:59:11 +0400

    Initial Commit

---
+++ NativeAvatar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NativeAvatar
+//
+//  Created by Роман Крендясов on 24.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
